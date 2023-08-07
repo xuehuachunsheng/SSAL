93f65dde435b06d921f26d07b26f2a7d954c7e9b
xuehuachunsheng <39287692+xuehuachunsheng@users.noreply.github.com>
2023-08-07 17:14:03 +0800

    Update run.sh

--- FashionMNIST/run.sh
+++ FashionMNIST/run.sh
@@ -1,8 +1,14 @@
 echo "Running FashionMNIST"
-
 cd /home/wyx/vscode_projects/SSAL/FashionMNIST
 nohup /home/wyx/env/python388/bin/python3 -u /home/wyx/vscode_projects/SSAL/FashionMNIST/train.py \
-    --gpu 1 \
+    --gpu 0 \
     --info U \
+    --balance True \
+    > log.out 2>&1 &
+
+cd /home/wyx/vscode_projects/SSAL/FashionMNIST
+nohup /home/wyx/env/python388/bin/python3 -u /home/wyx/vscode_projects/SSAL/FashionMNIST/train_sup.py \
+    --gpu 1 \
+    --info F \
     --balance True \
     > log.out 2>&1 &
