ff68482043230c7fde650dee3df6c2bd365def99
xuehuachunsheng <39287692+xuehuachunsheng@users.noreply.github.com>
2023-08-07 17:13:32 +0800

    Add files via upload

--- CIFAR100/run.sh
+++ CIFAR100/run.sh
@@ -7,3 +7,9 @@ nohup /home/wyx/env/python388/bin/python3 -u /home/wyx/vscode_projects/SSAL/CIFA
     --balance True \
     > log.out 2>&1 &
 
+cd /home/wyx/vscode_projects/SSAL/CIFAR100
+nohup /home/wyx/env/python388/bin/python3 -u /home/wyx/vscode_projects/SSAL/CIFAR100/train_sup.py \
+    --gpu 1 \
+    --info F \
+    --balance True \
+    > log.out 2>&1 &
