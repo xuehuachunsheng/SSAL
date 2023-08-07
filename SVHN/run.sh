20278338b20923eced5409c3c3939d3d81067d0d
xuehuachunsheng <39287692+xuehuachunsheng@users.noreply.github.com>
2023-08-07 17:14:27 +0800

    Update run.sh

--- SVHN/run.sh
+++ SVHN/run.sh
@@ -1,8 +1,14 @@
 echo "Running SVHN"
 cd /home/wyx/vscode_projects/SSAL/SVHN
 nohup /home/wyx/env/python388/bin/python3 -u /home/wyx/vscode_projects/SSAL/SVHN/train.py \
-    --gpu 1 \
+    --gpu 0 \
     --info F \
     --balance True \
     > log.out 2>&1 &
 
+cd /home/wyx/vscode_projects/SSAL/SVHN
+nohup /home/wyx/env/python388/bin/python3 -u /home/wyx/vscode_projects/SSAL/SVHN/train_sup.py \
+    --gpu 1 \
+    --info F \
+    --balance True \
+    > log.out 2>&1 &
